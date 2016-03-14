2dc48d4840e3ef0a60e476d82d6bab8c98760aca
Larry Ogrodnek <ogrodnek@gmail.com>
2016-03-13 22:17:45 -0700

    cache headers

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/bin/bash -e
 
 R=`dirname $0`
 
 rm -rf $R/.build
-cactus build && aws s3 sync $R/.build s3://engineers.coffee
+cactus build
+
+set -x
+aws s3 sync $R/.build s3://engineers.coffee --exclude "*.mp3" --cache-control max-age=300
+aws s3 sync $R/.build s3://engineers.coffee --include "*.mp3" --cache-control max-age=600
