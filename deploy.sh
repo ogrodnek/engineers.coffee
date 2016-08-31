3037a02aa777c820a45f600646cee53d87273406
Larry Ogrodnek <ogrodnek@gmail.com>
2016-08-30 21:27:43 -0700

    run CF invalidation

--- deploy.sh
+++ deploy.sh
@@ -18,3 +18,23 @@ set -x
 aws s3 sync $R/.build s3://engineers.coffee --exclude "*.mp3" --cache-control max-age=600 --profile analog
 
 aws s3 cp $R/.build/${epath} s3://engineers.coffee/${epath} --cache-control max-age=1800 --profile analog
+
+
+# Cloudfront invalidation
+INVALIDATION_ID=$(date +"%s")
+INVALIDATION_JSON="{
+    \"DistributionId\": \"ETIZ8M4U1N92S\",
+    \"InvalidationBatch\": {
+        \"Paths\": {
+            \"Quantity\": 3,
+            \"Items\": [
+                \"/rss.xml\",
+                \"/index.html\",
+		\"/\"
+            ]
+        },
+        \"CallerReference\": \"$INVALIDATION_ID\"
+    }
+}"
+
+aws cloudfront create-invalidation --cli-input-json "$INVALIDATION_JSON" --profile analog
