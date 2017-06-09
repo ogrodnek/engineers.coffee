d1416fa992bd48b0f6ebf6ed6da0820d2b4e31d1
Larry Ogrodnek <ogrodnek@gmail.com>
2017-06-09 11:13:07 -0700

    32, 33

--- plugins/blog.py
+++ plugins/blog.py
@@ -7,7 +7,7 @@ from django.template import Context
 from django.template.loader import get_template
 from django.template.loader_tags import BlockNode, ExtendsNode
 
-Global = {"config": {}, "posts": [] }
+Global = {"config": {}, "posts": []}
 
 Global["config"]["path"] = "episodes"
 Global["config"]["date_format"] = "%m-%d-%Y %I:%M%p"
@@ -15,7 +15,7 @@ Global["config"]["post_body_block"] = "body"
 
 
 def preBuild(site):
-	
+
 	global Global
 
 	# Check if the posts path exists
@@ -25,9 +25,9 @@ def preBuild(site):
 		logging.warning("No posts folder found at: %s", page_path)
 
 	for page in site.pages():
-		
+
 		if page.path.startswith("%s/" % Global["config"]["path"]):
-			
+
 			if not page.path.endswith('.html'):
 				continue
 
@@ -38,11 +38,11 @@ def preBuild(site):
 
 			# Check if we have the required keys
 			for field in ["title", "date", "episode", "subtitle", "duration", "length"]:
-				
+
 				if not context.has_key(field):
 					logging.warning("Page %s is missing field: %s" % (page.path, field))
 				else:
-					
+
 					if field == "date":
 						context_post[field] = _convertDate(context[field], page.path)
 					else:
@@ -56,11 +56,11 @@ def preBuild(site):
 
 			# Add the post contents
 			context_post["body"] = _get_node(
-				get_template(page.path), 
-				context=temp_post_context, 
+				get_template(page.path),
+				context=temp_post_context,
 				name=Global["config"]["post_body_block"])
-                
-            
+
+
 
 			Global["posts"].append(context_post)
 
@@ -69,20 +69,20 @@ def preBuild(site):
 	Global["posts"].reverse()
 
 	indexes = xrange(0, len(Global["posts"]))
-	
+
 	for i in indexes:
 		if i+1 in indexes: Global["posts"][i]['prevPost'] = Global["posts"][i+1]
 		if i-1 in indexes: Global["posts"][i]['nextPost'] = Global["posts"][i-1]
 
 
 def preBuildPage(site, page, context, data):
-	
+
 	context['posts'] = Global["posts"]
-	
+
 	for post in Global["posts"]:
 		if post["path"] == page.path:
 			context.update(post)
-	
+
 	return context, data
 
 
@@ -90,8 +90,8 @@ def preBuildPage(site, page, context, data):
 
 def _convertDate(date_string, path):
 	# Convert a string to a date object
-	try: 
-		return datetime.datetime.strptime(date_string, 
+	try:
+		return datetime.datetime.strptime(date_string,
 			Global["config"]["date_format"])
 	except Exception, e:
 		logging.warning("Date format not correct for page %s, should be %s\n%s" \
